98cd03ebe726802906a305ae9b5b4ce856eca1b6
Michael Allan <michaelallan@optushome.com.au>
2012-09-21 06:45:12 +1000

    Begin handling input in Ch 5 file explorer

--- ch5-1-file-explorer/index.js
+++ ch5-1-file-explorer/index.js
@@ -41,10 +41,15 @@ fs.readdir(__dirname, function (err, files) {
     }
 
     function option(data) {
-        if (!files[Number(data)]) {
+        var filename = files[Number(data)];
+        if (!filename) {
             stdout.write('    \033[31mEnter your choice: \033[39m');
         } else {
             stdin.pause();
+            fs.readFile(__dirname + '/' + filename, 'utf8', function (err, data) {
+                console.log('');
+                console.log(' \033[90m' + data.replace(/(.*/g, '    $1') + '\033[39m');
+            });
         }
     }
 
